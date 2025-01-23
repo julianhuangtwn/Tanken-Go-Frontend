f6ff3d6306734316e1a8aa8b65f9af1dfb66cb7b
Kaung Khant Zaw <kaung516177@gmail.com>
2025-01-23 15:32:26 -0500

    LoginForm design added

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,61 +1,68 @@
-"use client";
-
-import Link from "next/link";
-
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
 import {
-  CardTitle,
+  Card,
+  CardContent,
   CardDescription,
   CardHeader,
-  CardContent,
-  CardFooter,
-  Card,
-} from "@/components/ui/card";
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-
-export function LoginForm() {
+export function LoginForm({
+  className,
+  ...props
+}) {
   return (
-    <div className="w-full max-w-md">
-      <form>
-        <Card>
-          <CardHeader className="space-y-1">
-            <CardTitle className="text-3xl font-bold">Sign In</CardTitle>
-            <CardDescription>
-              Enter your details to sign in to your account
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="identifier"
-                name="identifier"
-                type="text"
-                placeholder="username or email"
-              />
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <Button variant="outline" className="w-full">
+                Login with Google
+              </Button>
             </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="password"
-              />
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
             </div>
-          </CardContent>
-          <CardFooter className="flex flex-col">
-            <button className="w-full">Sign In</button>
-          </CardFooter>
-        </Card>
-        <div className="mt-4 text-center text-sm">
-          Don't have an account?
-          <Link className="underline ml-2" href="signup">
-            Sign Up
-          </Link>
-        </div>
-      </form>
+          </form>
+        </CardContent>
+      </Card>
     </div>
-  );
+  )
 }
