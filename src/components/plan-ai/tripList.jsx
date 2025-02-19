8c5f64a8423342ba4a9f2bc75b70eada191359c8
KisungC <chung.kisung0@gmail.com>
2025-02-18 23:24:52 -0500

    changed url to .envs

--- src/components/plan-ai/tripList.jsx
+++ src/components/plan-ai/tripList.jsx
@@ -25,7 +25,7 @@ export default function TripList({setIsMapOpen}) {
 
       try {
         const response = await fetch(
-          `http://localhost:8080/v1/image?query=${encodeURIComponent(query)}&orientation=landscape`,
+          `${process.env.NEXT_PUBLIC_API_URL}/v1/image?query=${encodeURIComponent(query)}&orientation=landscape`,
           {
             method: "GET",
             headers: {
