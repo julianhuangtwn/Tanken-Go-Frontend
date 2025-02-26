5c4e44d32e5e55c520009d75c8b12723c5574193
julianhuangtwn <juliantwn@gmail.com>
2025-02-25 19:27:19 -0500

    Centered UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,36 @@
 // src/components/Navbar.js
-"use client"; // Add this directive at the top of the file
+"use client";
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { travel } from './ui/fonts'
-
+import { travel } from './ui/fonts';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <Link href='/'>
-        <div className="flex logo items-center">
+    <nav className="navbar flex justify-between items-center px-8 py-4 w-full mx-auto">
+      {/* Left Logo */}
+      <div className="logo flex-shrink-0">
+        <Link href="/">
           <h1 className={`${travel.className} antialiased text-6xl`}>Tanken-GO</h1>
-        </div>
-      </Link>
-      <ul className="flex items-center nav-links text-center">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/explore">Explore Trips</Link>
-        </li>
-        <li>
-          <Link href="/community">Community</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
+        </Link>
+      </div>
+
+      {/* Centered Navigation Links */}
+      <ul className="nav-links flex items-center text-center">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/explore">Explore Trips</Link></li>
+        <li><Link href="/community">Community</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
       </ul>
-      <div className="nav-right">
-        <button className="start-button"><Link href="/explore">Start Planning</Link></button>
-        <button className="account-button"><Link href="/login">Login</Link></button>
+
+      {/* Right Buttons */}
+      <div className="nav-right flex space-x-4 justify-end">
+        <Link href="/explore">
+          <button className="start-button">Start Planning</button>
+        </Link>
+        <Link href="/login">
+          <button className="account-button">Login</button>
+        </Link>
       </div>
     </nav>
   );
