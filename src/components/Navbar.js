0ad4fc1b747e5a3742f4bcf071a51f90df86c97f
Kaung Khant Zaw <kaung516177@gmail.com>
2025-03-18 18:32:13 -0400

    Navbar adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,31 +32,55 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex items-center justify-between min-h-18 p-2 shadow-md" >
-      <Link href='/'>
-        <div className="flex items-center font-bold mr-10">
-          <h1 className={`${travel.className} antialiased text-5xl`}>Tanken-GO</h1>
-        </div>
-      </Link>
-      <div className="flex flex-grow justify-center text-xl text-center items-center space-x-16 mx-4">
+    <nav className="navbar" >
+      <div className="navbar-container">
+        <Link href='/'>
+          <div className="flex items-center font-bold mr-10">
+            <h1 className={`${travel.className} antialiased text-5xl`}>Tanken-GO</h1>
+          </div>
+        </Link>
+
+        <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </button>
+        <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+          <li>
             <Link href="/">Home</Link>
+          </li>
+          <li>
             <Link href="/explore">Explore Trips</Link>
+          </li>
+          <li>
             <Link href="/community">Community</Link>
-            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/guide">Guide</Link>
+          </li>
+          <li>
             <Link href="/account/details">Account</Link>
-      </div>
-      <div className="nav-right flex items-center space-x-4">
-        { user.isLoggedIn ? (
-          <>
-            <button className="rounded-lg bg-themePink text-white text-lg font-bold px-6 py-3 border-themePink border-2"><Link href="/explore">Start Planning</Link></button>
-            <button className="rounded-lg text-themePink text-lg font-bold px-6 py-3 border-themePink border-2" onClick={handleLogout}>Logout</button>
-          </>
-        ) : (
-          <>
-            <button className="rounded-lg bg-themePink text-white text-lg font-bold px-6 py-3 border-themePink border-2"><Link href="/explore">Start Planning</Link></button>
-            <button className="rounded-lg text-themePink text-lg font-bold px-6 py-3 border-themePink border-2"><Link href="/login">Login</Link></button>
-          </>
-        )}
+          </li>
+        </ul>
+
+        {/* <div className="flex flex-grow justify-center text-xl text-center items-center space-x-16 mx-4">
+              <Link href="/">Home</Link>
+              <Link href="/explore">Explore Trips</Link>
+              <Link href="/community">Community</Link>
+              <Link href="/contact">Contact</Link>
+              <Link href="/account/details">Account</Link>
+        </div> */}
+        <div className="nav-right flex items-center space-x-4">
+          { user.isLoggedIn ? (
+            <>
+              <button className="rounded-lg bg-themePink text-white text-lg font-bold px-6 py-3 border-themePink border-2"><Link href="/explore">Start Planning</Link></button>
+              <button className="rounded-lg text-themePink text-lg font-bold px-6 py-3 border-themePink border-2" onClick={handleLogout}>Logout</button>
+            </>
+          ) : (
+            <>
+              <button className="rounded-lg bg-themePink text-white text-lg font-bold px-6 py-3 border-themePink border-2"><Link href="/explore">Start Planning</Link></button>
+              <button className="rounded-lg text-themePink text-lg font-bold px-6 py-3 border-themePink border-2"><Link href="/login">Login</Link></button>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
